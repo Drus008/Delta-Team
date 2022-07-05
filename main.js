dab7257e5486c827a4748d1c25f3b7d889b978c2
Drus008 <drussentis@gmail.com>
2022-07-05 12:33:49 +0200

    Zoom+subir
    He conseguido un zoom más suave y épico con el uso de matrices

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ let zoom = 1;
 const ZOOM_SPEED = 0.1;
 
 document.addEventListener("wheel", function (e) {
-  if (e.deltaY > 0) {
-    zoom += ZOOM_SPEED
-    zoomElement.style.transform = `scale(${(zoom)})`;
-    console.log(e.deltaY);
-  } else if (zoom > 0.2) {
-    zoom -= ZOOM_SPEED
-    zoomElement.style.transform = `scale(${(zoom)})`;
-    console.log(e.deltaY)
-  }
-});
+	if (e.deltaY > 0) {
+		zoom += ZOOM_SPEED
+		zoomElement.style.transform = `matrix( ${(zoom**2)}, 0, 0, ${(zoom**2)}, 0, ${(-(zoom-1)*200)} )`;
+		console.log(e.deltaY);
+		console.log(zoom);
+		}
+		else if (zoom > 0.2) {
+		zoom -= ZOOM_SPEED
+		zoomElement.style.transform = `matrix( ${(zoom**2)}, 0, 0, ${(zoom**2)}, 0, ${(-(zoom-1)*200)} )`;
+		console.log(e.deltaY)
+	}
+	console.log(e.deltaY);
+	console.log(zoom);
+  });
