174c533ba853ec25af30db06f94176499d7ad3ef
Drus008 <drussentis@gmail.com>
2022-07-05 17:38:26 +0200

    Comentarios + svg
    Se han añadido comentarios y aumentado la calidad de las imágenes

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+/*Elije que elemento se apmpliará y define como de rápido*/
 const zoomElement = document.querySelector("#contenedor-logo");
 let zoom = 1;
 const ZOOM_SPEED = 0.35;
 
+/*Aplica una transformación lineal cada vez que mueves la rueda del ratón*/
 document.addEventListener("wheel", function (e) {
 	if (e.deltaY > 0) {
 		zoom += ZOOM_SPEED
-		zoomElement.style.transform = `matrix( ${(zoom**2)}, 0, 0, ${(zoom**2)}, 0, ${(-(zoom-1)*200)} )`;
+		zoomElement.style.transform = `matrix( ${(zoom**2)}, 0, 0, ${(zoom**2)}, ${((zoom-1)*100)}, ${(-(zoom-1)*200)} )`;
 		console.log(e.deltaY);
 		console.log(zoom);
 		}
 		else if (zoom > 0.2) {
 		zoom -= ZOOM_SPEED
-		zoomElement.style.transform = `matrix( ${(zoom**2)}, 0, 0, ${(zoom**2)}, 0, ${(-(zoom-1)*200)} )`;
+		zoomElement.style.transform = `matrix( ${(zoom**2)}, 0, 0, ${(zoom**2)}, ${((zoom-1)*100)}, ${(-(zoom-1)*200)} )`;
 		console.log(e.deltaY)
 	}
 	console.log(e.deltaY);
